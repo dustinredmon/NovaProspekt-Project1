7798bc4014090444667cbf4d0cbe9a92c4421d83
dustinredmon <54919395+dustinredmon@users.noreply.github.com>
2019-10-23 15:41:33 -0700

    Create main.tf
    first version of main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+    backend  "s3" {
+    region         = "us-west-2"
+    bucket         = "novaprospekt-bucket"
+    key            = "npkey/terraform.tfstate"
+    dynamodb_table = "tf-state-lock"
+    }
+}
+
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_s3_bucket" "tf-remote-state" {
+  bucket = "novaprospekt-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-tf-state-lock" {
+  name            = "tf-state-lock"
+  hash_key        = "LockID"
+  read_capacity   = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "DynamoDB tf state locking"
+  }
+}
