72c46db0e8d3309f8d929c5f9cbc163f68bb4f95
dustinredmon <54919395+dustinredmon@users.noreply.github.com>
2019-11-02 00:50:49 -0700

    Create route53.tf
    A records for apex and www domain

---
+++ tf/route53.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.external.zone_id}"
+  name    = "www.novaprospekt.xyz"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_elb.np-elb.dns_name}"
+    zone_id                = "${aws_elb.np-elb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "apex" {
+  zone_id = "${aws_route53_zone.external.zone_id}"
+  name    = "novaprospekt.xyz"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_route53_record.www.dns_name}"
+    zone_id                = "${aws_route53_record.www.zone_id}"
+    evaluate_target_health = true
+  }
+}
