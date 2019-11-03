1e140c273453a1edd072bba7af907acd1b74ff7b
qusad <shtyrtsvlad@gmail.com>
2019-11-03 12:39:53 -0800

    week 3 update

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "bastion" {
 	ami = "ami-06d51e91cea0dac8d"
 	instance_type = "t2.micro"
-	key_name = "${aws_key_pair.mykey.key_name}"
+	key_name = "${aws_key_pair.key_pair.key_name}"
 	subnet_id = "${aws_subnet.bastion-1-public.id}"
 
 	vpc_security_group_ids = ["${aws_security_group.bastion-sg.id}"]
@@ -14,13 +14,15 @@ resource "aws_instance" "bastion" {
 resource "aws_instance" "server-1" {
 	ami = "ami-06d51e91cea0dac8d"
 	instance_type = "t2.micro"
+	key_name = "${aws_key_pair.key_pair.key_name}"
 	subnet_id = "${aws_subnet.server-1-private.id}"
-	vpc_security_group_ids = ["${aws_security_group.bastion-sg.id}"]
+	vpc_security_group_ids = ["${aws_security_group.server-sg.id}"]
 }
 
 resource "aws_instance" "server-2" {
 	ami = "ami-06d51e91cea0dac8d"
 	instance_type = "t2.micro"
+	key_name = "${aws_key_pair.key_pair.key_name}"
 	subnet_id = "${aws_subnet.server-2-private.id}"
-	vpc_security_group_ids = ["${aws_security_group.bastion-sg.id}"]
+	vpc_security_group_ids = ["${aws_security_group.server-sg.id}"]
 }
--- tf/nat.tf
+++ tf/nat.tf
@@ -15,28 +15,28 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "nat-gate" {
 	allocation_id = "${aws_eip.nat.id}"
 	subnet_id = "${aws_subnet.main-1-public.id}"
-	depends_on = ["aws_internet_gateway.main-natgate"]
+	depends_on = ["aws_internet_gateway.main-gate"]
 }
 
-resource "aws_route_table" "main-private-nat" {
+resource "aws_route_table" "main-table-private" {
 	vpc_id = "${aws_vpc.main_vpc.id}"
 	route {
 		cidr_block = "0.0.0.0/0"
 		nat_gateway_id = "${aws_nat_gateway.nat-gate.id}"
 	}
 	tags = {
-		Name = "main-private-nat"
+		Name = "main-table-private"
 	}
 }
 
 resource "aws_route_table_association" "main-private-1" {
 	subnet_id = "${aws_subnet.server-1-private.id}"
-	route_table_id = "${aws_route_table.main-private-nat.id}"
+	route_table_id = "${aws_route_table.main-table-private.id}"
 }
 
 resource "aws_route_table_association" "main-private-2" {
         subnet_id = "${aws_subnet.server-2-private.id}"
-        route_table_id = "${aws_route_table.main-private-nat.id}"
+        route_table_id = "${aws_route_table.main-table-private.id}"
 } 
 
 
