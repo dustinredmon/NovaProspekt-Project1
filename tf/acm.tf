757b58edef3054736da2ecee4b6e1ce562faee06
dustinredmon <54919395+dustinredmon@users.noreply.github.com>
2019-11-03 18:53:25 -0800

    Update acm.tf
    trying something else

--- tf/acm.tf
+++ tf/acm.tf
@@ -3,7 +3,6 @@ resource "aws_acm_certificate" "default" {
   validation_method = "DNS"
   subject_alternative_names = [
     "www.novaprospekt.xyz",
-    "*.novaprospekt.xyz",
   ]
 }
 
@@ -20,18 +19,10 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_route53_record" "cert_validation_alt1" {
-  name    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_type}"
+  name    = "${aws_acm_certificate.default.domain_validation_options.1.resource_record_name}"
+  type    = "${aws_acm_certificate.default.domain_validation_options.1.resource_record_type}"
   zone_id = "${data.aws_route53_zone.external.id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.1.resource_record_value}"]
-  ttl     = 60
-}
-
-resource "aws_route53_record" "cert_validation_alt2" {
-  name    = "${aws_acm_certificate.cert.domain_validation_options.2.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.2.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.external.id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.2.resource_record_value}"]
+  records = ["${aws_acm_certificate.default.domain_validation_options.1.resource_record_value}"]
   ttl     = 60
 }
 
