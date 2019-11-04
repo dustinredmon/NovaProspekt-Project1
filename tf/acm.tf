b9e5f400444bf984ac52b9c9fc8943f9e35d6254
dustinredmon <54919395+dustinredmon@users.noreply.github.com>
2019-11-03 18:42:03 -0800

    Update acm.tf
    added 2 more validations

--- tf/acm.tf
+++ tf/acm.tf
@@ -19,6 +19,22 @@ resource "aws_route53_record" "validation" {
   ttl     = "60"
 }
 
+resource "aws_route53_record" "cert_validation_alt1" {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.external.id}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.1.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_route53_record" "cert_validation_alt2" {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.2.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.2.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.external.id}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.2.resource_record_value}"]
+  ttl     = 60
+}
+
 resource "aws_acm_certificate_validation" "default" {
   certificate_arn = "${aws_acm_certificate.default.arn}"
   validation_record_fqdns = [
