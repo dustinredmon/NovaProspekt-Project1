8b7d72a0cfc3e60fbdd259400baf115c74395790
dustinredmon <54919395+dustinredmon@users.noreply.github.com>
2019-11-03 18:32:50 -0800

    Update route53.tf
    testing domain name

--- tf/route53.tf
+++ tf/route53.tf
@@ -20,7 +20,7 @@ resource "aws_route53_record" "apex" {
   type    = "A"
 
   alias {
-    name                   = "${aws_route53_record.www.dns_name}"
+    name                   = "www.novaprospekt.xyz"
     zone_id                = "${aws_route53_record.www.zone_id}"
     evaluate_target_health = true
   }
